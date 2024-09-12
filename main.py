3c50fb454aaf6edd49da75f7028c27b19df09d96
men_l <men_long@yahoo.com>
2024-09-12 13:55:00 -0400

    Upload this document onto my Github

--- main.py
+++ main.py
@@ -7,7 +7,25 @@ import os
 import time
 
 
-def addition()
+def addition():
+    """This function asks the user to enter a series of numbers separated by spaces. It then adds all the numbers
+    together and returns the result."""
+    nums = list(map(int,(input("Enter all numbers separately by space: ").split())))
+    return sum(nums)
+# print(addition())
 
+def subtraction():
+    """This function asks the user to enter a series of number separated by spaces. It then subtracts the second
+     number from the first and return the result."""
+    # nums_1 = float(input("Enter the first number"))
+    # nums_2 = float(input ("Enter the second number"))
+    # return nums_1 - nums_2
+    nums = list(map(int, input("Enter all numbers separately by space: ").split()))
+    result = nums[0]
+    for num in nums[1:]:
+        result -=num
+    return result
+
+print(subtraction())
 
 
